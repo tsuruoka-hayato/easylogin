b1ca63f535072a42a59aff23b25643d6a0cce085
hayatotsuruoka <kurokokagamitt36@gmail.com>
2024-05-29 12:09:40 +0900

    easylogin init

---
+++ src/test/java/jp/cp/internous/easylogin/EasyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cp.internous.easylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
